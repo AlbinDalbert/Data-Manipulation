f4223e9c9dce994ec2be40e08f5aad470ee2fa8c
Albin Dalbert <albin.dalbert.all@gmail.com>
2022-04-27 20:56:32 +0200

    Added vigenere cipher

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Data-Manipulation
+
+- [ ]   Add config file for things such as the alphabet
+
+- [x]   caesar
+- [ ]   vigenere
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,6 @@ pub mod caesar;
 fn main() {
     
     let mut system = System::new("Encode".to_string(), Some(TermColor::Green), None);
-
-    //let p = program::Program::new("prog".to_string(), run, None, 0);
     
     system.add_program("Caesar".to_string(),caesar::caesar_run, None);
     
