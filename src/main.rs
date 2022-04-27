dfd49763329ddcffc413340e524c8f64b3d8432c
Albin Dalbert <albin.dalbert.all@gmail.com>
2022-04-27 21:02:16 +0200

    fixes with Vigenere

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use terminal_toolbox::{*, system::*};
 
+use crate::vigenere::vigenere_run;
+
 pub mod caesar;
+pub mod vigenere;
 
 fn main() {
     
     let mut system = System::new("Encode".to_string(), Some(TermColor::Green), None);
     
     system.add_program("Caesar".to_string(),caesar::caesar_run, None);
+    system.add_program("Vigenere".to_string(), vigenere_run, None);
     
     system.menu();
 
--- src/vigenere.rs
+++ src/vigenere.rs
@@ -15,8 +15,8 @@ pub fn vigenere_run(){
     
     // let the user choose the mode of operation (encrypt or decrypt)
     println!("{}", match mode.to_lowercase().as_str() {
-        "1" => vigenere_encrypt(&text, key),
-        "2" => vigenere_decrypt(&text, key),
+        "1" => vigenere_encrypt(&text, &key),
+        "2" => vigenere_decrypt(&text, &key),
         _ => {
             println!("Invalid mode");
             exit(1);
@@ -54,12 +54,11 @@ fn vigenere_decrypt(text: &str, key: &str) -> String {
         if c.is_ascii_lowercase() {
             // lowercase
             let index = ALPHABET_LOWER.find(c).unwrap();
-            new_c = ALPHABET_LOWER.chars().nth((index - ALPHABET_LOWER.find(key.chars().nth(key_index).unwrap()).unwrap()) % 26).unwrap();
         }
         else if c.is_ascii_uppercase() {
             // uppercase
             let index = ALPHABET_UPPER.find(c).unwrap();
-            new_c = ALPHABET_UPPER.chars().nth((index - ALPHABET_UPPER.find(key.chars().nth(key_index).unwrap()).unwrap()) % 26).unwrap();
+
         }
         key_index = (key_index + 1) % key.len();
         result.push(new_c);
